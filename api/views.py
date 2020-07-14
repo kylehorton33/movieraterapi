138169f85dc722b78c8c4a1d1782671f420c6264
Kyle <kylehorton33@gmail.com>
2020-07-13 21:00:13 -0400

    video 37: rating update validator, return custom results when updating rating

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -13,10 +14,41 @@ class MovieViewSet(viewsets.ModelViewSet):
 
   @action(detail=True, methods=['POST'])
   def rate_movie(self, request, pk=None):
-    response = {
-      'message' : 'its working'
-    }
-    return Response(response, status=status.HTTP_200_OK)
+    if 'stars' in request.data:
+
+      movie = Movie.objects.get(id=pk)
+      stars = request.data['stars']
+      #user = request.user
+      user = User.objects.get(id=1)
+      print('user', user.username)
+      print('movie title', movie.title)
+
+      try:
+        rating = Rating.objects.get(user=user.id, movie=movie.id)
+        rating.stars = stars
+        rating.save()
+
+        serializer = RatingSerializer(rating, many=False)
+        response = {
+          'message' : 'Rating updated',
+          'result' : serializer.data
+        }
+        return Response(response, status=status.HTTP_200_OK)
+        
+      
+      except:
+        Rating.objects.create(user=user, movie=movie, stars=stars)
+        serializer = RatingSerializer(rating, many=False)
+        response = {
+          'message' : 'Rating created',
+          'result' : serializer.data
+        }
+
+    else:
+      response = {'message' : 'You need to provide stars'}
+      return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    
 
 class RatingViewSet(viewsets.ModelViewSet):
   queryset = Rating.objects.all()
