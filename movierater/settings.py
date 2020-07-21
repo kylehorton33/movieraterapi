1a0ec13b36eef58488ed27332aea8fd73cbb02aa
Kyle <kylehorton33@gmail.com>
2020-07-20 21:36:43 -0400

    fixed bug from register user feature

--- movierater/settings.py
+++ movierater/settings.py
@@ -91,9 +91,10 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : {
-        'rest_framework.permissions.IsAuthenticated',
-    }
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # Password validation
